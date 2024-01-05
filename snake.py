9a0929afa058a237c377c99da6a5f4ad0c49a1b3
Ed Haig-Thomas <ehaigthomas@gmail.com>
2024-01-05 12:49:17 +0000

    Added a snake texture and fixed food spawning bug

--- snake.py
+++ snake.py
@@ -1,7 +1,18 @@
 from config import *
 from piece import Piece
 
+def generate_food(snake, grid_size):
+  valid_pos = False
+  while not valid_pos:
+    food = Piece(randint(0,900/grid_size), randint(0,900/grid_size), 0, grid_size)
+    for snake_piece in snake:
+      if not food.x == snake_piece.x and food.y == snake_piece.y:
+        valid_pos = True
+  return food
+
 exit = False
+nhs = False
+hi_score = int(open('hi-scores.txt', 'r').readline().strip())
 grid_size = 30
 cube_size = 30
 menu = False
@@ -25,8 +36,10 @@ while not exit:
   # Menu                                     
   if menu:
     fps = 10
-    index = 0                                      
-    menu_dimensions = pygame.Rect(screen.get_width()/2, screen.get_height()/3, 300, 300)
+    index = 0
+    title = font.render('Snake', True, '#ffffff')                               
+    menu_dimensions = pygame.Rect(screen.get_width()/2-150, screen.get_height()/2-150, 300, 300)
+    screen.blit(title, (menu_dimensions.x, menu_dimensions.y-70))
     menu_rect = pygame.draw.rect(screen, (240,240,240), menu_dimensions, 2)
     for menu_option in menu_options:
       index += 30
@@ -65,6 +78,7 @@ while not exit:
       if event.key == K_RIGHT and menu:
         if selected == 0:
           menu = False
+          nhs = False
           
       if event.key == K_LEFT and not menu:
         if snake[0].dir+2 != 3:
@@ -81,10 +95,16 @@ while not exit:
       
   # Snake
   if not menu:
+    colour_lg = True
     head = pygame.Rect(snake[0].x*grid_size, snake[0].y*grid_size, cube_size-1, cube_size-1)
     for index, snake_piece in enumerate(snake):
       segment = pygame.Rect(snake_piece.x*grid_size, snake_piece.y*grid_size, cube_size, cube_size)
-      pygame.draw.rect(screen, (0, 255-index*2, 0), segment)
+      if colour_lg:
+        pygame.draw.rect(screen, (0, 255, 0), segment)
+        colour_lg = False
+      else:
+        pygame.draw.rect(screen, (0, 150, 0), segment)
+        colour_lg = True
       
       if head.colliderect(segment) and index != 0:
         death = True
@@ -110,7 +130,6 @@ while not exit:
         fps = 8
         menu = True 
         death = False
-        score = 0
         cube_size = grid_size
         snake = [Piece(1, 4, 1, grid_size), Piece(2, 4, 1, grid_size)]
 
@@ -121,17 +140,17 @@ while not exit:
     pygame.draw.rect(screen, (255, 0, 0), food_rect)
 
     if head.colliderect(food_rect):
-      snake.append(Piece(snake[len(snake)-1].x, snake[len(snake)-1].y, snake[len(snake)-1].dir, grid_size))
-      valid_pos = True
       score += 1
-      while valid_pos:
-        food = Piece(randint(10,900/grid_size), randint(10,900/grid_size), 0, grid_size)
-        for snake_piece in snake:
-          if not (food.x == snake_piece.x and food.y == snake_piece):
-            valid_pos = False
-        
+      snake.append(Piece(snake[len(snake)-1].x, snake[len(snake)-1].y, snake[len(snake)-1].dir, grid_size))
+      food = generate_food(snake, grid_size)
+      
+     
     # Score
     score_font = font.render(f"{score}", True, (255,255,255))
+    if score > hi_score:
+      open('hi-score.txt', 'r').write(score)
+      hi_score = score
+      nhs = True
     screen.blit(score_font, (430,0))
 
   pygame.display.flip() # display the frame
